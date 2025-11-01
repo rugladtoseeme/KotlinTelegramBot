b7799e6bad0d3c6b7096c2dc4d2476e35372c96f
rugladtoseeme <mrgrafen@mail.ru>
2025-11-01 20:46:19 +0300

    KTB-05-menu создать меню для консольного приложения при помощи while

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,15 +2,36 @@ package org.example
 
 import java.io.File
 
+const val WORDS_FILE_NAME = "words.txt"
+
 data class Word(val original: String, val translation: String, var correctAnswersCount: Int = 0)
 
-fun main() {
-    val file = File("words.txt")
-
+fun loadDictionary(): MutableList<Word> {
+    val file = File(WORDS_FILE_NAME)
     val dictionary = mutableListOf<Word>()
     val lines = file.readLines()
     for (line in lines) {
         val wordParts = line.split('|')
         dictionary.add(Word(wordParts[0], wordParts[1], wordParts[2].toIntOrNull() ?: 0))
     }
+    return dictionary
+}
+
+fun main() {
+
+    val dictionary = loadDictionary()
+
+    println("Меню: \n1 – Учить слова\n2 – Статистика\n0 – Выход")
+    while (true) {
+        val choice = readln().toIntOrNull()
+        when {
+            choice == 1 -> println("Учить слова")
+            choice == 2 -> println("Статистика.")
+            choice == 0 -> break
+            else -> println("Введите число 1, 2 или 0")
+        }
+    }
+
+    println("Работа завершена.")
+
 }
