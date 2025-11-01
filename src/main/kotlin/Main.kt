bee89e1886a333c6195010734ca3e68137ef2397
rugladtoseeme <mrgrafen@mail.ru>
2025-11-01 14:52:56 +0300

    KTB-02-count-right-answers добавить поле для подсчета числа правильных ответов, считать файл в список

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,15 @@ package org.example
 
 import java.io.File
 
+data class Word(val original: String, val translation: String, var correctAnswersCount: Int = 0)
+
 fun main() {
     val file = File("words.txt")
-    val words = file.readLines()
-    for (word in words) {
-        println(word)
+
+    val dictionary = mutableListOf<Word>()
+    val lines = file.readLines()
+    for (line in lines) {
+        val wordParts = line.split('|')
+        dictionary.add(Word(wordParts[0], wordParts[1], wordParts[2].toIntOrNull() ?: 0))
     }
 }
