3272d45ff95cbb2096122ab7ae91921849d96598
rugladtoseeme <mrgrafen@mail.ru>
2025-11-13 18:21:41 +0300

    KTB-09-refactor рафакторинг кода

---
+++ src/main/kotlin/LearnWordsTrainer.kt
@@ -0,0 +1,74 @@
+package org.example
+
+import java.io.File
+
+data class Word(val original: String, val translation: String, var correctAnswersCount: Int = 0) {
+    fun toDictionaryFormat() = "${original}|${translation}|${correctAnswersCount}"
+}
+
+data class Statistics(
+    val learned: Int,
+    val total: Int,
+    val percent: Double = (learned.toDouble() / (if (total == 0) 1 else total)) * 100
+)
+
+data class Question(val correctAnswer: Word, val variants: List<Word>) {
+
+    fun questionToString(): String {
+        val variantsStr =
+            variants.mapIndexed { index, word -> " ${index + 1} - ${word.translation}" }.joinToString("\n")
+        return "\n${correctAnswer.original}:\n" + variantsStr + "\n ---------\n 0 - выйти в меню"
+    }
+}
+
+class LearnWordsTrainer {
+
+    private var currentQuestion: Question? = null
+    private var dictionary = loadDictionary()
+
+    fun getNextQuestion(): Question? {
+        val notLearnedWords = dictionary.filter { it.correctAnswersCount < RIGHT_ANSWERS_THRESHOLD }
+        if (notLearnedWords.isEmpty()) return null
+        val variants = notLearnedWords.shuffled().take(QUESTION_WORDS_SIZE)
+        currentQuestion = Question(variants.random(), variants)
+        return currentQuestion
+    }
+
+    fun checkAnswer(answerIndex: Int): Boolean {
+        return currentQuestion.let {
+            if (answerIndex in (0..(currentQuestion?.variants?.size ?: 0)) &&
+                currentQuestion?.correctAnswer?.translation == currentQuestion?.variants[answerIndex]?.translation
+            ) {
+                currentQuestion?.correctAnswer?.correctAnswersCount++
+                saveDictionary(dictionary)
+                true
+            } else false
+        }
+    }
+
+    fun getStatistics(): Statistics {
+        val totalCount = dictionary.size
+        val learnedCount = dictionary.filter { it.correctAnswersCount >= RIGHT_ANSWERS_THRESHOLD }.count()
+        return Statistics(learnedCount, totalCount)
+    }
+
+    private fun loadDictionary(): MutableList<Word> {
+        val file = File(WORDS_FILE_NAME)
+        val lines = file.readLines()
+        val dictionary = mutableListOf<Word>()
+        for (line in lines) {
+            val wordParts = line.split('|')
+            dictionary.add(Word(wordParts[0], wordParts[1], wordParts[2].toIntOrNull() ?: 0))
+        }
+        return dictionary
+    }
+
+    private fun saveDictionary(dictionary: List<Word>) {
+        val file = File(WORDS_FILE_NAME)
+        val lines = mutableListOf<String>()
+        for (word in dictionary) {
+            lines.add(word.toDictionaryFormat())
+        }
+        file.writeText(lines.joinToString("\n"))
+    }
+}
