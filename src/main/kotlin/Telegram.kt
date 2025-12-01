e1407ac2378bd2a52e7487c459c4d93032ca225d
rugladtoseeme <mrgrafen@mail.ru>
2025-12-01 21:58:50 +0300

    KTB-16-create-buttons разработать кнопки меню

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -9,6 +9,7 @@ fun main(args: Array<String>) {
     val updateIdRegex = "\"update_id\":\\s*(\\d+)".toRegex()
     val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
     val chatIdRegex = "\"chat\"\\s*:\\s*\\{\\s?\"id\"\\s*:\\s*(\\d+)".toRegex()
+    val dataRegex = "\"data\":\"(.+?)\"".toRegex()
 
     while (true) {
         val updates = tgBotService.getUpdates(updateId)
@@ -19,11 +20,30 @@ fun main(args: Array<String>) {
         val chatId: Long = chatIdRegex.find(updates)?.groups?.get(1)?.value?.toLongOrNull() ?: continue
 
         val text = messageTextRegex.find(updates)?.groups?.get(1)?.value
+
+        val data = dataRegex.find(updates)?.groups?.get(1)?.value
+
         println(text)
         Thread.sleep(2000)
 
         if (text.equals("hello", ignoreCase = true)) {
             val response = tgBotService.sendMessage(chatId, "hello!")
+        }
+
+        if (text.equals("menu", ignoreCase = true)) {
+            val response = tgBotService.sendMenu(chatId)
+        }
+
+        if (data.equals("words_learning_cliched", ignoreCase = true)) {
+            val response = tgBotService.sendMessage(
+                chatId, "Учить английские слова",
+            )
+        }
+
+        if (data.equals("statistics_clicked", ignoreCase = true)) {
+            val response = tgBotService.sendMessage(
+                chatId, "Выучено 10 из 10 слов | 100%"
+            )
         }
     }
 }
